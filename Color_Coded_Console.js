14e061074f08a2644cdf65d1e170f0952bd7e8a8
L1nqz <58988050+L1nqz@users.noreply.github.com>
2020-12-09 08:15:50 -0800

    Create Color_Coded_Console.js

---
+++ Color_Coded_Console.js
@@ -0,0 +1,38 @@
+console.log("\u001b[30m Black message");
+console.log("\u001b[31m Red message");
+console.log("\u001b[32m Green message");
+console.log("\u001b[33m Yellow message");
+console.log("\u001b[34m Blue message");
+console.log("\u001b[35m Magenta message");
+console.log("\u001b[36m Cyan message");
+console.log("\u001b[0m Reset color");
+console.log("--------------------------");
+console.log("\u001b[30;1m Bright Black message");
+console.log("\u001b[31;1m Bright Red message");
+console.log("\u001b[32;1m Bright Green message");
+console.log("\u001b[33;1m Bright Yellow message");
+console.log("\u001b[34;1m Bright Blue message");
+console.log("\u001b[35;1m Bright Magenta message");
+console.log("\u001b[36;1m Bright Cyan message");
+console.log("\u001b[0m Reset color");
+console.log("--------------------------")
+console.log("\u001b[40m Black background");
+console.log("\u001b[41m Red background");
+console.log("\u001b[42m Green background");
+console.log("\u001b[43m Yellow background");
+console.log("\u001b[44m Blue background");
+console.log("\u001b[45m Magenta background");
+console.log("\u001b[46m Cyan background");
+console.log("\u001b[47m White background");
+console.log("\u001b[0m Reset background (same as Reset color)");
+console.log("--------------------------")
+console.log("\u001b[40;1m Bright Black background");
+console.log("\u001b[41;1m Bright Red background");
+console.log("\u001b[42;1m Bright Green background");
+console.log("\u001b[43;1m Bright Yellow background");
+console.log("\u001b[44;1m Bright Blue background");
+console.log("\u001b[45;1m Bright Magenta background");
+console.log("\u001b[46;1m Bright Cyan background");
+console.log("\u001b[47;1m Bright White background");
+console.log("\u001b[0m Reset background (same as Reset color)");
+console.log("--------------------------")
